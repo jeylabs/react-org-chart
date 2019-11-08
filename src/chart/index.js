418ba0a0fa30632d615dd00c15816cfecc71d7dc
Majushan manokaran <mano.ceymplon@jeylabs.com.au>
2019-11-08 12:42:44 +0530

    Zoom enabled

--- src/chart/index.js
+++ src/chart/index.js
@@ -106,7 +106,7 @@ function init(options) {
 
   // Defined zoom behavior
   const zoom = d3.behavior
-    .zoom(false)
+    .zoom()
     // Define the [zoomOutBound, zoomInBound]
     .scaleExtent([0.4, 2])
     .duration(50)
